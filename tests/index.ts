c341271f2e42bba024859eb8fa78db19b57eef43
Jake 'Sid' Smith <jake@dabapps.com>
2021-03-17 21:15:04 +0000

    Test isNotNumber

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,4 @@
-import { isNumber } from '../src';
+import { isNumber, isNotNumber } from '../src';
 
 const self = <T>(input: T): T => input;
 
@@ -43,5 +43,49 @@ describe('isNumber', () => {
     }
 
     expect(isNumber(input)).toBe(false);
+  });
+});
+
+describe('isNotNumber', () => {
+  it('should return true when input is not a number', () => {
+    const input = null as number | null | undefined;
+
+    self(input); // @tsassert: number | null | undefined
+
+    if (isNotNumber(input)) {
+      self(input); // @tsassert: null | undefined
+    } else {
+      self(input); // @tsassert: number
+    }
+
+    expect(isNotNumber(input)).toBe(true);
+  });
+
+  it('should return false when input is a number', () => {
+    const input = 0 as number | null | undefined;
+
+    self(input); // @tsassert: number | null | undefined
+
+    if (isNotNumber(input)) {
+      self(input); // @tsassert: null | undefined
+    } else {
+      self(input); // @tsassert: number
+    }
+
+    expect(isNotNumber(input)).toBe(false);
+  });
+
+  it('should narrow to type never when types do not overlap', () => {
+    const input = '' as string | null | undefined;
+
+    self(input); // @tsassert: string | null | undefined
+
+    if (isNotNumber(input)) {
+      self(input); // @tsassert: string | null | undefined
+    } else {
+      self(input); // @tsassert: never
+    }
+
+    expect(isNotNumber(input)).toBe(true);
   });
 });
